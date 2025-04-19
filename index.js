21b075016e0cb56b31a9f31c6e75048ae907241b
Tammy Higgins <tmhiggins1986@gmail.com>
2025-04-18 19:27:13 -0500

    Quiz game

---
+++ index.js
@@ -0,0 +1,32 @@
+import inquirer from "inquirer";
+import { quiz } from "./quizQs";
+
+const {playerName} = await inquirer.prompt({
+  type: "input",
+  name: "playerName",
+  message: "What's your name?",
+  default: "Player1",
+});
+
+console.log(`Hello, ${playerName}Lets begin the quiz!`);
+
+let score = 0;
+
+for (let i = 0; i < quiz.length; i++) {
+  const { question, answer, choices } = quiz[i];
+
+  const { userAnswer } = await inquirer.prompt({
+    type: "list",
+    name: "userAnswer",
+    message: "question",
+    choices,
+  });
+
+  if (userAnswer === answer) {
+    console.log("Nice work!");
+    score++;
+  } else {
+    console.log("Wrong! , The correct answer is" + answer);
+  }
+}
+console.log("You scored".score);
---
+++ quizQs.js
@@ -0,0 +1,19 @@
+"use strict";
+
+export const quiz = [
+  {
+    question: "Whats the capital of France?",
+    choices: ["Berlin", "Madrid", "Paris", "Rome"],
+    answer: "Paris",
+  },
+  {
+    question: "Which language runs in the browser?",
+    choices: ["Java", "C", "Python", "Javascript"],
+    answer: "Javascript",
+  },
+  {
+    question: "What year did node.js first appear?",
+    choices: ["2005", "2000", "2001", "2024"],
+    answer: "2000",
+  },
+];
